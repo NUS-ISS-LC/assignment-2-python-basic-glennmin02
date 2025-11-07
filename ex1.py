5e0025cb1badfe9e042cfcc71f9ae961905fbe60
Glenn Min <113816638+glennmin02@users.noreply.github.com>
2025-11-07 07:14:35 +0000

    Name: Kaung Zayar Min

--- ex1.py
+++ ex1.py
@@ -1,3 +1,7 @@
 def find(s, n):
-# write your implementation here
-    return None
+    for i in range (len(s)):
+        for j in range (i+1, len(s)):
+             if s[i] + s[j] == n:
+                 return [i,j]
+
+print (find([1,3,4,5,1,11,4,1],9))
